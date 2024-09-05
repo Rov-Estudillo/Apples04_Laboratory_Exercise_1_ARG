a48b7352e14808f059b2d5e25b638ebecc5b15eb
Rov-Estudillo <rvnll.029@gmail.com>
2024-09-05 08:40:52 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal pricePerApple = 32.50m;
+
+        Console.Write("Enter the number of apples you want to purchase: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int qtyOfApples))
+        {
+            decimal totalPrice = qtyOfApples * pricePerApple;
+            Console.WriteLine($"The total price of {qtyOfApples} is {totalPrice}.");
+
+            int convertedPrice = (int)Math.Floor(totalPrice);
+            Console.WriteLine($"The value of the converted price is {convertedPrice}.");
+        }
+    }
+}
